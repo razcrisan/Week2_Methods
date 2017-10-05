aa6c320dac40d8827f87d1e819dc4f687db0f256
razcrisan <rcrisan13@gmail.com>
2017-10-05 09:56:23 -0400

    first successful method

--- Week2_Methods/Program.cs
+++ Week2_Methods/Program.cs
@@ -8,8 +8,43 @@ namespace Week2_Methods
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //    int fNum = int.Parse(Console.ReadLine());
+            //    int sNum = int.Parse(Console.ReadLine());
+
+
+            //    Console.WriteLine(Add(fNum, sNum));
+            //}
         }
+        //public static int Add(int firstNumber, int secondNumber)
+        //{
+        //    int sum = firstNumber + secondNumber;
+
+        //    return sum;
+        //    //is not like a consoleWriteLine, just holds it           
+
+
+        //}
+        public static void FavFood(string name, string food)
+        {
+            Console.WriteLine(name + " " + food);
+
+        }
+
+        public static int RetirementCalculator(int age, int rAge)
+        {
+            rAge = 65;
+            age = int.Parse(Console.ReadLine());
+            int retirementAge = rAge - age;
+            
+            return retirementAge;       
+        }
+
+        
     }
-}
+
+    }
+
+
