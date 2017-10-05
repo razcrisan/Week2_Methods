2c43e4cc194910b94352e6aa5c710c301c2c9418
razcrisan <rcrisan13@gmail.com>
2017-10-05 14:02:46 -0400

    getting the hang of methods

--- Week2_Methods/Program.cs
+++ Week2_Methods/Program.cs
@@ -17,32 +17,81 @@ namespace Week2_Methods
 
             //    Console.WriteLine(Add(fNum, sNum));
             //}
+
+           
+            //Console.WriteLine("Please enter ur name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("please enter ur fav food");
+            //string food = Console.ReadLine();
+            //FavFood(name, food);
+            //FavFood("Bob", "Pizza");
+            //FavFood("raz", "burger");
+            //FavFood("Moni", "shakes");
+
+            
+            //retirementcalculator(60);
+
+            Console.WriteLine("Please type in your hours worked");
+            double hoursWorked = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please type in your hourly wage");
+            double hourlyWage = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(WageCalculator(hoursWorked, hourlyWage));
+
+
+
+
+            Console.WriteLine("How much did you make this month?");
+            double income = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How much did you spend this month?");
+            double expenses = double.Parse(Console.ReadLine());
+
+            SavingsCalculator(income, expenses);
         }
+
         //public static int Add(int firstNumber, int secondNumber)
         //{
         //    int sum = firstNumber + secondNumber;
-
-        //    return sum;
-        //    //is not like a consoleWriteLine, just holds it           
-
-
+        //    return sum;          
         //}
-        public static void FavFood(string name, string food)
-        {
-            Console.WriteLine(name + " " + food);
 
+
+        //public static void FavFood(string name, string food)
+        //{
+        //    Console.WriteLine("**********************************");          
+        //    Console.WriteLine(name + "'s favorite food is " + food);
+        //    Console.WriteLine();
+        //}
+
+        //public static void retirementcalculator(int age)
+        //{
+        //    int retirementage = Math.Max(0, 65 - age);
+        //    Console.WriteLine("The user will retire in " + retirementage +" years");         
+        //}
+
+        public static double WageCalculator(double hoursWorked, double hourlyWage)
+        {
+            double moneyMade = hoursWorked * hourlyWage;           
+            return moneyMade;
         }
 
-        public static int RetirementCalculator(int age, int rAge)
+        public static double SavingsCalculator( double income, double expenses)
         {
-            rAge = 65;
-            age = int.Parse(Console.ReadLine());
-            int retirementAge = rAge - age;
-            
-            return retirementAge;       
+            if (income > expenses)
+            {
+                double savings = (income - expenses) * .15;
+                Console.WriteLine("you should set aside $" + savings + " this month");
+                return savings;
+            }
+            else
+            {
+                Console.WriteLine("Nothing to save this month. Easy on the Starbucks");
+                return 0;
+            }
         }
 
-        
+
     }
 
     }
